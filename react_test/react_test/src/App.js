60af0b6868f8811b6884f75e59112ff607ca9844
chathudu sahassara <88680508+IT20253912@users.noreply.github.com>
2023-07-06 21:17:46 +0530

    complete props revision
    with object pass, by paramiterize

--- react_test/react_test/src/App.js
+++ react_test/react_test/src/App.js
@@ -1,12 +1,29 @@
 
 import './App.css';
+import Main from './Components/Main';
 
+const name = "name test";
+const position = "position test";
+
+const myObj = {
+  name:`${name}`,
+  position:`${position}`
+}
 function App() {
-  return (
-    <div>
 
-    
+  return (
+    <>
+    <div>
+        chathudu
+        <Main />
+        <Main />
+        <Main>
+          <p>{name}</p>
+          <p>{myObj.position}</p>
+        </Main>
+        <Main name="saman" position="SE" city="colombo"/>
     </div>
+    </>
   );
 }
 
---
+++ react_test/react_test/src/Components/Main.js
@@ -0,0 +1,17 @@
+function Main({name , position , city}){
+    console.log(city);
+    return(
+        <div>
+            Hello World! 
+        <ul>
+            <li>{name}</li>
+            <li>{position}</li>
+            <li>{city}</li>
+        </ul>
+             
+        </div>
+    );
+
+
+}
+export default Main;
