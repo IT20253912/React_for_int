e2a370b063c6e1f5b49e0393738d549a94690dd3
chathudu sahassara <88680508+IT20253912@users.noreply.github.com>
2023-07-07 07:24:24 +0530

    new day

--- react_test/react_test/src/App.js
+++ react_test/react_test/src/App.js
@@ -3,12 +3,15 @@ import Main from "./Components/Main";
 
 import myData from "./Data/myData";
 
-const Mainblock = myData.map(({name , city , position, id}) => {
-  return <Main key={id} name={name} city={city} position={position}/>;
-});
+
 
 
 function App() {
+
+  const Mainblock = myData?.map(({name , city , position, id}) => {
+    return <Main key={id} name={name} city={city} position={position}/>;
+  });
+
   console.log(myData);
 
   return (
--- react_test/react_test/src/Components/Main.js
+++ react_test/react_test/src/Components/Main.js
@@ -2,12 +2,11 @@ function Main({name , position , city}){
     // console.log(city);
     return(
         <div>
-            Hello World! 
-        <ul>
-            <li>{name}</li>
-            <li>{position}</li>
-            <li>{city}</li>
-        </ul>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKiN_J0bIU38iblibITf_o9iekv4d2dGRNmQ&usqp=CAU" alt="card" />
+            <h3>{name}</h3>
+            <p><span>{position}</span><span>{city}</span></p>
+           
+        
         </div>
     );
 
