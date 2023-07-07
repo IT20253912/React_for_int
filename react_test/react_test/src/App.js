dc67c922c9ef5e17248c71144c4019e81afdfb26
chathudu sahassara <88680508+IT20253912@users.noreply.github.com>
2023-07-07 23:54:29 +0530

    part 5 50.14

--- react_test/react_test/src/App.js
+++ react_test/react_test/src/App.js
@@ -1,28 +1,63 @@
+import React, { useState } from "react";
 import "./App.css";
 import Main from "./Components/Main";
-
 import myData from "./Data/myData";
 
-
-
-
 function App() {
-
-  const Mainblock = myData?.map(({name , city , position, id}) => {
-    return <Main key={id} name={name} city={city} position={position}/>;
+  const Mainblock = myData?.map(({ name, city, position, id }) => {
+    return <Main key={id} name={name} city={city} position={position} />;
   });
 
-  console.log(myData);
+  const [myVar, setmyVar] = useState({
+    name: "nameOne",
+    city: "cityOne",
+    position: "positionOne",
+  });
+
+  const [input, setInput] = useState('');
+
+  const clickHandle = () => {
+    setmyVar((prevState) => ({
+      ...prevState,
+      name: "sahassara",
+    }));
+  };
+
+  console.log(input);
 
   return (
     <div className="main_container">
-    <div>
-      AUK learning center
-      <h1>chathudu</h1>
-    </div>
-    <div className="mainBlock_container">
-      {Mainblock}
-    </div>
+      <div>
+        AUK learning center
+        <h1>chathudu</h1>
+        <h2>{myVar.name}</h2>
+        <h2>{input}</h2>
+      </div>
+      <div className="mainBlock_container">{Mainblock}</div>
+      <br />
+      <br />
+      <button
+        style={{
+          fontSize: "12px",
+          border: "1px solid red",
+          padding: "7px 12px",
+        }}
+        onClick={clickHandle}
+      >
+        click me
+      </button>
+      <br />
+      <br />
+      <input
+        style={{
+          border: "1px solid red",
+          fontSize: "14px",
+          padding: "7px",
+        }}
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
     </div>
   );
 }
