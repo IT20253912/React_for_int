c069febb8f897db45b977e62b5ec8bfd0939ccf3
chathudu sahassara <88680508+IT20253912@users.noreply.github.com>
2023-07-08 17:26:47 +0530

    test commit

--- react_test/react_test/src/App.js
+++ react_test/react_test/src/App.js
@@ -1,9 +1,19 @@
 const App = () => {
-  return(
-    <div>
+  return (
+    <div className="main_container">
       <h1>chathudu</h1>
+      <div className="main_left">
+        
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+
+        <button>Submit</button>
+      </div>
+      <div className="main_right"></div>
     </div>
-  ); 
-}
+  );
+};
 
 export default App;
