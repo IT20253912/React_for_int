3176f30eedad0bd7f1420feddb4cbe5ee4144b3a
chathudu sahassara <88680508+IT20253912@users.noreply.github.com>
2023-07-15 07:20:09 +0530

    part 9 33:54

--- react_test/react_test/src/App.js
+++ react_test/react_test/src/App.js
@@ -1,12 +1,55 @@
 import { useEffect, useState } from "react";
-import './App.css';
+import "./App.css";
 import Unit from "./Components/Unit";
 import Header from "./Components/Header";
 
 const App = () => {
+  const [apiId, setapiId] = useState("1");
+  const [data, setdata] = useState({});
+
+  console.log(apiId);
+  console.log(data);
+
+  useEffect(() => {
+    // console.log("use effect running!!");
+    const apicall = async () => {
+      console.log("API call runnnig")
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${apiId}`
+      );
+      const data = await res.json();
+
+      if (data) {
+        setdata(data);
+      }
+    };
+
+    if (apiId.length > 0 && Number(apiId) > 0 && Number(apiId) <= 100) {
+        console.log("useEffect is running !!!");
+        apicall();
+    }
+
+    return () => {
+      console.log('clean up');
+      apicall();
+    }
+
+  }, [apiId]);
+
   return (
     <div>
-      <input type="text" placeholder="enter ID"/>
+      <input
+        type="text"
+        placeholder="enter ID"
+        value={apiId}
+        onChange={(e) => setapiId(e.target.value)}
+      />
+      {data && (
+        <div>
+          <h2>{data?.title}</h2>
+          <p>{data?.body}</p>
+        </div>
+      )}
     </div>
   );
 };
