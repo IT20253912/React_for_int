c916794ea5a2b7b3c929d65558b6baa2c3861a20
chathudu sahassara <88680508+IT20253912@users.noreply.github.com>
2023-07-16 19:37:27 +0530

    test input box with useState

--- react_test/react_test/src/App.js
+++ react_test/react_test/src/App.js
@@ -4,13 +4,20 @@ import Unit from "./Components/Unit";
 import Header from "./Components/Header";
 
 const App = () => {
+  const [apiId, setApiId] = useState("1");
+
+  console.log(apiId);
+
   return (
     <div>
-        <input type="text" placeholder="input number"/>
+      <input
+        type="text"
+        value={apiId}
+        onChange={(e) => setApiId(e.target.value)}
+        placeholder="input number"
+      />
     </div>
-  )
-}
+  );
+};
 
 export default App;
-
-
